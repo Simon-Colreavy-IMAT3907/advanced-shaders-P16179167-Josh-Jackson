476420240d5828964c6ef026d323e676a95ec174
Pjaerr <joshcharlesjackson@gmail.com>
2019-10-25 13:07:03 +0100

    Swapped out old implementation for GLUT code

--- Advanced-Shaders/Advanced-Shaders/main.cpp
+++ Advanced-Shaders/Advanced-Shaders/main.cpp
@@ -1,120 +1,74 @@
-#define GLEW_STATIC
-#include <GL/glew.h>		
-#include <GLFW/glfw3.h> 
-#include <stdio.h>
+#include <GL\glut.h>
 
-int main() {
-	GLFWwindow *window = NULL;
-	const GLubyte *renderer;
-	const GLubyte *version;
-	GLuint vao;
-	GLuint vbo;
-	/* geometry to use. these are 3 xyz points (9 floats total) to make a
-		 triangle */
-	GLfloat points[] = { 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
+GLfloat xRotated, yRotated, zRotated;
+GLdouble size = 1;
 
-	/* these are the strings of code for the shaders
-	the vertex shader positions each vertex point */
-	const char *vertex_shader = "#version 120\n"
-		"attribute vec3 vp;"
-		"void main () {"
-		"  gl_Position = vec4 (vp, 1.0);"
-		"}";
-	/* the fragment shader colours each fragment (pixel-sized area of the
-	triangle) */
-	const char *fragment_shader = "#version 120\n"
-		"void main () {"
-		"  gl_FragColor = vec4 (0.5, 0.0, 0.5, 1.0);"
-		"}";
-	/* GL shader objects for vertex and fragment shader [components] */
-	GLuint vs, fs;
-	/* GL shader programme object [combined, to link] */
-	GLuint shader_programme;
+void display(void);
+void reshape(int x, int y);
+void idle(void)
+{
 
-	/* start GL context and O/S window using the GLFW helper library */
-	if (!glfwInit()) {
-		fprintf(stderr, "ERROR: could not start GLFW3\n");
-		return 1;
-	}
+	xRotated += 0.01;
+	yRotated += 0.01;
+	zRotated += 0.01;
+	display();
+}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
-	window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
-	if (!window) {
-		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
-		glfwTerminate();
-		return 1;
-	}
-	glfwMakeContextCurrent(window);
-	// start GLEW extension handler
-	glewExperimental = GL_TRUE;
-	glewInit();
 
-	/* get version info */
-	renderer = glGetString(GL_RENDERER); /* get renderer string */
-	version = glGetString(GL_VERSION);	 /* version as a string */
-	printf("Renderer: %s\n", renderer);
-	printf("OpenGL version supported %s\n", version);
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitWindowSize(350, 350);
+	glutCreateWindow("Teapot");
+	xRotated = yRotated = zRotated = 30.0;
+	xRotated = 43;
+	yRotated = 50;
 
-	// tell GL to only draw onto a pixel if the shape is closer to the viewer
-	glEnable(GL_DEPTH_TEST); // enable depth-testing
-		// depth-testing interprets a smaller value as "closer"
-	glDepthFunc(GL_LESS);
-
-	/* a vertex buffer object (VBO) is created here. this stores an array of
-		 data on the graphics adapter's memory. in our case - the vertex points */
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), points, GL_STATIC_DRAW);
-
-	/* the vertex array object (VAO) is a little descriptor that defines which
-	data from vertex buffer objects should be used as input variables to vertex
-	shaders. in our case - use our only VBO, and say 'every three floats is a
-	variable' */
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
-	/* here we copy the shader strings into GL shaders, and compile them. we
-		 then create an executable shader 'program' and attach both of the compiled
-		 shaders. we link this, which matches the outputs of the vertex shader to
-		 the inputs of the fragment shader, etc. and it is then ready to use */
-	vs = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vs, 1, &vertex_shader, NULL);
-	glCompileShader(vs);
-	fs = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fs, 1, &fragment_shader, NULL);
-	glCompileShader(fs);
-	shader_programme = glCreateProgram();
-	glAttachShader(shader_programme, fs);
-	glAttachShader(shader_programme, vs);
-	glLinkProgram(shader_programme);
-
-	/* this loop clears the drawing surface, then draws the geometry described
-		 by the VAO onto the drawing surface. we 'poll events' to see if the window
-		 was closed, etc. finally, we 'swap the buffers' which displays our drawing
-		 surface onto the view area. we use a double-buffering system which means
-		 that we have a 'currently displayed' surface, and 'currently being drawn'
-		 surface. hence the 'swap' idea. in a single-buffering system we would see
-		 stuff being drawn
-	one-after-the-other */
-	while (!glfwWindowShouldClose(window)) {
-		// wipe the drawing surface clear
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glUseProgram(shader_programme);
-		glBindVertexArray(vao);
-		// draw points 0-3 from the currently bound VAO with current shader
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// update other events like input handling
-		glfwPollEvents();
-		// put the stuff we've been drawing onto the display
-		glfwSwapBuffers(window);
-	}
-
-	// close GL context and any other GLFW resources
-	glfwTerminate();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutIdleFunc(idle);
+	glutMainLoop();
 	return 0;
+}
+
+void display(void)
+{
+
+	glMatrixMode(GL_MODELVIEW);
+	// clear the drawing buffer.
+	glClear(GL_COLOR_BUFFER_BIT);
+	// clear the identity matrix.
+	glLoadIdentity();
+	// traslate the draw by z = -4.0
+	// Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
+	glTranslatef(0.0, 0.0, -5.0);
+	// Red color used to draw.
+	glColor3f(0.1, 0.3, 1.0);
+	// changing in transformation matrix.
+	// rotation about X axis
+	glRotatef(xRotated, 1.0, 0.0, 0.0);
+	// rotation about Y axis
+	glRotatef(yRotated, 0.0, 1.0, 0.0);
+	// rotation about Z axis
+	glRotatef(zRotated, 0.0, 0.0, 1.0);
+	// scaling transfomation 
+	glScalef(1.0, 1.0, 1.0);
+	// built-in (glut library) function , draw you a teapot.
+	glutWireTeapot(size);
+	// Flush buffers to screen
+
+	glFlush();
+	// sawp buffers called because we are using double buffering 
+   // glutSwapBuffers();
+}
+
+void reshape(int x, int y)
+{
+	if (y == 0 || x == 0) return;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(39.0, (GLdouble)x / (GLdouble)y, 0.6, 21.0);
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, x, y);  //Use the whole window for rendering
 }
